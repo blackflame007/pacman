bb38d4e527e91a9c8076ed1c7efa09327748d564
Nick L. <lesseosnick@gmail.com>
2023-11-12 13:53:02 -0800

    Fixd: ghost collecting and destroying dots on the board

--- gamestate/ghost.go
+++ gamestate/ghost.go
@@ -8,30 +8,36 @@ func (g *gameState) moveGhost(ghost *ghost.Ghost) {
 	newX := ghost.X + dx
 	newY := ghost.Y + dy
 
-	// Wraparound Pacman's position when moving through tunnels
+	// Wraparound logic for moving through tunnels
 	if newY < 0 {
 		newY = len(g.board.Cells) - 1
 	} else if newY >= len(g.board.Cells) {
 		newY = 0
-	} else if newX < 0 {
+	}
+	if newX < 0 {
 		newX = len(g.board.Cells[0]) - 1
 	} else if newX >= len(g.board.Cells[0]) {
 		newX = 0
 	}
 
+	// Check if the new position is not a wall and not another ghost
 	if g.board.Cells[newY][newX] != '#' && g.board.Cells[newY][newX] != 'G' {
-		// Save the current cell value before moving the ghost
-		previousCell := g.board.Cells[ghost.Y][ghost.X]
+		// Check if the current cell is a pellet, then restore it
+		if ghost.PreviousCell == '.' {
+			g.board.Cells[ghost.Y][ghost.X] = ghost.PreviousCell
+		}
+		if ghost.PreviousCell == 'O' {
+			g.board.Cells[ghost.Y][ghost.X] = ghost.PreviousCell
+		} else {
+			g.board.Cells[ghost.Y][ghost.X] = ' ' // Leave empty space
+		}
+
+		// Update PreviousCell with the content of the new cell
+		ghost.PreviousCell = g.board.Cells[newY][newX]
 
 		// Move the ghost to the new position
-		g.board.Cells[ghost.Y][ghost.X] = ' '
 		ghost.X = newX
 		ghost.Y = newY
 		g.board.Cells[ghost.Y][ghost.X] = 'G'
-
-		// Restore the previous cell value
-		if previousCell == '.' {
-			g.board.Cells[ghost.Y][ghost.X] = previousCell
-		}
 	}
 }
